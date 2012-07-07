5b3661e2f8e7902c689b655d46017c89887ffd6e
Yamamoto Yuji <whosekiteneverfly _at_ gmail.com>
2012-07-07 10:34:49 +0900

    Add required classes

--- tsv2dataset.py
+++ tsv2dataset.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python -3
 # -*- coding: utf-8 -*-
 
+class CobolFileSection:
+  def __init__(self, lines):
+    """Parse COBOL FILE SECTION"""
+    pass
+
+class  CobolPicture:
+  """Represent PICTURE phrase"""
+  def __init__(self,  data):
+    """Parse a line including PICTURE phrase."""
+     pass
+
+def tsv2dataset( tsv_reader, cob_lib ):
+  """Returns the converted records based on cob_file_section."""
+  pass
+
 def main(argv):
   pass
 
